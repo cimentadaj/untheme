7b0533af6d867868c578e7473f89d59e92387b65
Jorge Cimentada <cimentadaj@gmail.com>
2024-01-03 12:03:19 +0100

    Changes sample app to make use the plot_tabset

--- sample_app.R
+++ sample_app.R
@@ -1,52 +1,41 @@
-# File for testing currently being called from a Makefile
-
-devtools::load_all()
+library(untheme)
 library(shiny)
 library(shiny.semantic)
+library(ggplot2)
+library(plotly)
 
 ui <- fluidUnTheme(
-  titlePanel("Hello Shiny!"),
-  sidebar_layout(
-    sidebar_panel(
-      sliderInput("obs", "Number of observations:", min = 0, max = 1000, value = 500),
-      width = 3
-    ),
-    main_panel(
-      tags$head(
-        tags$style(HTML("
-          /* Scrollable tab container */
-          .scrollable-tabs {
-            overflow-x: auto;
-            white-space: nowrap;
-          }
-          /* Individual tabs */
-          .scrollable-tabs .item {
-            display: inline-block;
-            float: none;
-          }
-          /* Minimum width for plot container */
-          #plotContainer {
-            min-width: 1200px; /* Adjust this value as needed */
-          }
-        "))
-      ),
-      div(class = "scrollable-tabs", # Add this class
-        tabset(
-          list(
-            list(menu = "Some tab1", content = div(id = "plotContainer", plotOutput("distPlot"))),
-            list(menu = "Some tab1", content = plotOutput("distPlot")),
-            list(menu = "Some tab2", content = plotOutput("distPlot"))
-          )
-        )
-      )
+  tabset(
+    tabs = list(
+      create_tab("Tab 1", "plot1"),
+      create_tab("Tab 2", "plot2"),
+      create_tab("Tab 3", "plot3")
     )
   )
 )
 
-server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    hist(rnorm(input$obs))
+server <- function(input, output, session) {
+  data <- mtcars
+  ggplot_obj1 <- reactive({
+    gg <- ggplot(data, aes(x = mpg, y = wt)) + geom_point()
+    list(gg = gg, plotly = ggplotly(gg))
   })
+
+  ggplot_obj2 <- reactive({
+    gg <- ggplot(data, aes(x = mpg, y = cyl)) + geom_point()
+    list(gg = gg, plotly = ggplotly(gg))
+  })
+
+  ggplot_obj3 <- reactive({
+    gg <- ggplot(data, aes(x = mpg, y = disp)) + geom_point()
+    list(gg = gg, plotly = ggplotly(gg))
+  })
+
+  plots_tabset(
+    list(plt_reactive = ggplot_obj1, filename = "plot1"),
+    list(plt_reactive = ggplot_obj2, filename = "plot2"),
+    list(plt_reactive = ggplot_obj3, filename = "plot3")
+  )
 }
 
 shinyApp(ui, server)
